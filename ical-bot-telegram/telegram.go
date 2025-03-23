89ac7a589e5b1728d7c645b3301ed8c101c56a0c
kyomawolf <kyomawolf@kyomawolf.eu>
2025-03-23 17:12:51 +0100

    inital telegrambot commit

---
+++ ical-bot-telegram/telegram.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"google.golang.org/grpc"
+
+	icalbot "github.com/patrick246/ical-bot/ical-bot-backend/pkg/api/pb/ical-bot-backend/v1"
+)
+
+// Send any text message to the bot after the bot has been started
+
+const endpoint = "localhost:8081"
+const error_message = "ERROR: Telegram-Bot:"
+const info_message = "INFO: Telegram-Bot:"
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	botOpts := []bot.Option{
+		bot.WithDefaultHandler(defaultHandler),
+	}
+
+	b, err := bot.New(os.Getenv("EXAMPLE_TELEGRAM_BOT_TOKEN"), botOpts...)
+	if nil != err {
+		// panics for the sake of simplicity.
+		// you should handle this error properly in your code.
+		panic(err)
+	}
+
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/hello", bot.MatchTypeExact, helloHandler)
+
+	grpcOpts := []grpc.DialOption{}
+
+	clientConnection, err := grpc.NewClient("", grpcOpts...)
+
+	if err != nil {
+		println("%s grpc client connection could not be established: %s", error_message, err.Error())
+		defer clientConnection.Close()
+		os.Exit(1)
+	}
+	telegramClientBot := icalbot.NewIcalBotServiceClient(clientConnection)
+
+	stream, err := telegramClientBot.StreamEventNotifications(context.Background())
+
+	if err != nil {
+		println("%s telegram event stream notification could not be received: %s", error_message, err.Error())
+		defer clientConnection.Close()
+		os.Exit(1)
+	}
+
+	wait := make(chan *icalbot.EventNotification)
+	// grpc message waiter
+	go func() {
+		for {
+
+			notification, err := stream.Recv()
+			if err != nil {
+				println("%s notification could not be received: %s", error_message, err.Error())
+				return
+			}
+			ack := &icalbot.EventNotificationAcknowledge{}
+			ack.Id = notification.Id
+
+			wait <- notification
+			stream.Send(ack)
+		}
+	}()
+
+	// telegram bot sender
+	go func() {
+		for {
+			message := <-wait
+			if message == nil {
+				println("%s message is nil", error_message)
+			}
+			println("%s received notification with id: %s", info_message, message.Id)
+		}
+	}()
+	//routine {
+	// <-wait
+	//}
+	// when to use this?
+	// stream.CloseSend()
+
+	b.Start(ctx)
+}
+
+func helloHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    update.Message.Chat.ID,
+		Text:      "Hello, *" + bot.EscapeMarkdown(update.Message.From.FirstName) + "*",
+		ParseMode: models.ParseModeMarkdown,
+	})
+}
+
+func defaultHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   "Say /hello",
+	})
+}
